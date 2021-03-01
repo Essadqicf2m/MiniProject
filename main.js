c1b42a62fc7c7340281e0acfc066ade05100fb64
Essadqicf2m <webdev2020.essadqi@gmail.com>
2021-03-01 08:18:19 +0100

    first commit

--- main.js
+++ main.js
@@ -10,14 +10,34 @@ class Note
 
     get titre(){
         return this._titre;
-    } 
+    }
+    
     get date(){
         return this._date;
-    } 
+    }
+    
     get couleur(){
         return this._couleur;
     }
-
+    
+    set titre(titre){
+        if(typeof titre === "string"){
+            this._titre = titre;  
+        }
+    }
+    
+    set date(date){
+        if(typeof date === "string"){
+            this._date = date;
+        }
+    }
+    
+    set couleur(couleur){
+        if(typeof couleur === "string"){
+            this._couleur = couleur;
+        }   
+    }
+    
     displayNote(){
     
     
